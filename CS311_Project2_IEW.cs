4c8032d2d98ab5885011f4d0fe06234cf9cd3d35
IsaiahejWashington <isaiahejwashington@gmail.com>
2024-10-31 11:07:49 -0400

    Add files via upload

--- CS311_Project2_IEW.cs
+++ CS311_Project2_IEW.cs
@@ -1,73 +1,94 @@
-namespace Craps
+using System.Data.SqlTypes;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace Craps1
 {
-    public partial class FrmCraps : Form
+    public partial class Form1 : Form
     {
-        public FrmCraps()
+        public Form1()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
+
+        }//end textBox1
+
+        private void lbl3_Click(object sender, EventArgs e)
+        {
+            float money = 100;
+            money--;
+            money++;
+
+
+        }// end lbl3
+
+        private void btnRoll_Click(object sender, EventArgs e)
+        {
+            float money = 100;
+            money--;
+            txtBoxBet.Text = money.ToString("C");
             calculate_score();
-        }//end label1
+
+        }//end btnRoll 
+
+        private void lblDice1_Click(object sender, EventArgs e)
+        {
+
+            calculate_score();
+        }//end lbl Dice1
 
         private void lblDice2_Click(object sender, EventArgs e)
         {
             calculate_score();
-            ;
-        }//end lblDice2
+        }//end lbl Dice2
 
-        private void FrmCraps_Load(object sender, EventArgs e)
-        {
-
-        }//end FrmCraps
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            int balance = 100;
-            int bet =0;
-            Console.WriteLine(bet -= balance);
-        }//end textBox1
-
-        private void lblBankBalance_Click(object sender, EventArgs e)
-        {
-            int balance = 100;
-        }//end lblBankBalance
-
-        private void btnRoll_Click(object sender, EventArgs e)
+        private void lblWin_Click(object sender, EventArgs e)
         {
             calculate_score();
-        }//end btnRoll
 
+        }//end lblWin
 
+        private void lblLose_Click(object sender, EventArgs e)
+        {
+            calculate_score();
+        }//end lblLose
 
-
+        private void lblPoint_Click(object sender, EventArgs e)
+        {
+            calculate_score();
+        }//end lblPoint
 
         private void calculate_score()
         {
+            Random rand = new Random();
+            int dice1 = rand.Next(1, 7);
+            int dice2 = rand.Next(1, 7);
+            int sum = dice1 + dice2;
 
-            Random rnd = new Random();
-            int num1 = rnd.Next();
-            for (int d = 0; d < num1; d++)
+            String outcomeMessage;
+            switch (sum)
             {
-                Console.WriteLine(rnd.Next(1, 6));
-            }
+                case 7:
+                case 11:
+                    outcomeMessage = "You win!";
+                    break;
 
-            int num2 = rnd.Next();
-            for (int d = 0; d < num2; d++)
-            {
-                Console.WriteLine(rnd.Next(1, 6));
-            }
+                case 2:
+                case 3:
+                case 12:
+                    outcomeMessage = "You Lose!";
+                    break;
+
+                default:
+                    outcomeMessage = "Roll again!";
+                    break;
+            }//end calculate_score
 
 
-            Console.WriteLine(num1 -= num2);
 
 
-
-        }//end calculate_score
-
-        
-    }//end class 
-
+        }
+    }//end class
 }//end namespace
